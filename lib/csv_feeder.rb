c4a2c6478b57eb4ab751d7291824bf91873e1bf2
Braulio Carreno <bcarreno@yahoo.com>
2021-05-28 18:01:52 -0400

    Fix: the argument to rand should be 1 or bigger

--- lib/csv_feeder.rb
+++ lib/csv_feeder.rb
@@ -6,7 +6,14 @@ class TourBus
       @data = CSV.read(options['file'], headers: true)
     end
     def get_example
-      @data[rand(@data.length - 1)]
+      index = if @data.length == 1
+                0
+              elsif @data.length > 1
+                rand(@data.length - 1)
+              else
+                raise ArgumentError.new("CsvFeeder#get_example: there should be at least 1 line with data")
+              end
+      @data[index]
     end
   end
 end
